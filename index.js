78a3b38cd6d9e375c7037ec678c64757ca1fa970
daxtersky <daxtersky@gmail.com>
2020-08-07 08:25:49 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 // Import stylesheets
 import './style.css';
 
-document.addEventListener('DOMContentLoaded', function() {
-  console.log('domcontenttloaded')
-})
-
-// Write Javascript code!
-// const appDiv = document.getElementById('app');
-// appDiv.innerHTML = `<h1>JS Starter</h1>`;
 const buttonBox = document.getElementById('button-box');
 
 buttonBox.addEventListener('click', (e) => {
